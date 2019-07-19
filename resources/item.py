3035047fe148ed55c833aaa934413d834b3798c8
Marcelo Ceolin <fxsobr@gmail.com>
2019-07-19 09:48:37 -0300

    ajustes

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,4 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
 from models.item import ItemModel
 
 
@@ -17,7 +16,6 @@ class Item(Resource):
                         help="Every item needs a store id"
                         )
 
-    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -39,7 +37,6 @@ class Item(Resource):
 
         return item.json(), 201
 
-    @jwt_required()
     def delete(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -47,7 +44,6 @@ class Item(Resource):
             return {'message': 'Item deleted'}
         return {'message': 'Item not found.'}, 404
 
-    @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
